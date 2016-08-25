312dc8dc782b42789ef2ac666284875fea2e02c9
성민경 <zoz7@naver.com>
2016-08-25 21:35:47 +0900

    메세지를 보내기 위한 권한 설정 (Manifests)

---
+++ app/src/main/java/ensharp/goldensignal/Send_MMS.java
@@ -0,0 +1,13 @@
+package ensharp.goldensignal;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class Send_MMS extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send__mms);
+    }
+}
