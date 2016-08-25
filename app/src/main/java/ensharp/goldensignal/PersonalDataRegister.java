dc48dc68169438f388bdc652c61bf3fa31d4c369
안태현 <antaehyeon@live.co.kr>
2016-08-25 19:51:23 +0900

    초기 상태

---
+++ app/src/main/java/ensharp/goldensignal/PersonalDataRegister.java
@@ -0,0 +1,13 @@
+package ensharp.goldensignal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PersonalDataRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_data_register);
+    }
+}
