a002110a82a6279a7d40e32951f7802dacec0b7b
성민경 <zoz7@naver.com>
2016-08-25 21:41:14 +0900

    사고가 났을때, 신고 문자를 보내주는 부분, 버튼을 클릭하면 SendSMS 를 통해 문자를 보낸다 (현재: 나의 정보 설정 값과 연동 되지 않아, 임시로 개발자의 정보를 입력해 뒀다 나중에 고쳐야 할부분 // 최종으로 합칠때, sendSMS() 를 이용하는게 아닌, 버튼 클릭 이벤트로 사용가 추가권한 설정과 함께 바꿔줘야 하는 부분)

--- app/src/main/java/ensharp/goldensignal/Send_MMS.java
+++ app/src/main/java/ensharp/goldensignal/Send_MMS.java
@@ -1,13 +1,99 @@
 package ensharp.goldensignal;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Send_MMS extends ActionBarActivity {
+    private Context mContext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send__mms);
+
+        mContext = Send_MMS.this;
+
+    }
+
+    public void sendSMS(View v){
+
+        String strName = "사고자 이름";
+        String myPhoneNumber = "01048862255";
+
+
+        //String strPhone = phone.getText().toString();
+        //String strAddress = address.getText().toString();
+
+        String total = "사고자 : " + strName + '\n' + "연락처 : " + myPhoneNumber + '\n' + "사고가 났습니다. 도와주세요.";
+
+        if (total.length()>0 ){ //smsNum.length()>0 &&smsText.length()>0
+            sendSMS("01048862255", total);
+        }else{
+            Toast.makeText(this, "모두 입력해 주세요", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void sendSMS(String smsNumber, String total){
+        PendingIntent sentIntent = PendingIntent.getBroadcast(this, 0, new Intent("SMS_SENT_ACTION"), 0);
+        PendingIntent deliveredIntent = PendingIntent.getBroadcast(this, 0, new Intent("SMS_DELIVERED_ACTION"), 0);
+
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch(getResultCode()){
+                    case Activity.RESULT_OK:
+                        Toast.makeText(mContext, "SMS 전송 완료", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(mContext, "SMS 전송 실패", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(mContext, "서비스 지역이 아닙니다", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(mContext, "무선(Radio)가 꺼져있습니다", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(mContext, "PDU Null", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter("SMS_SENT_ACTION"));
+
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (getResultCode()){
+                    case Activity.RESULT_OK:
+                        Toast.makeText(mContext, "SMS 도착완료", Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(mContext, "SMS 도착안됨", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter("SMS_DELIVERED_ACTION"));
+
+        SmsManager mSmsManager = SmsManager.getDefault();
+        mSmsManager.sendTextMessage("01048862255", null, total, sentIntent, deliveredIntent);
+
+        // 지정 글자수 넘어갔을때 mms로 보내도록 //
+        ArrayList<String> messageParts = mSmsManager.divideMessage(total);
+
+        mSmsManager.sendMultipartTextMessage("01048862255", null, messageParts, null, null);
+
+        Toast.makeText(this, "MMS 전송완료.", Toast.LENGTH_SHORT).show();
     }
 }
