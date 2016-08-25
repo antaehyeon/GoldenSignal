8f87021717c3a6fc7c2c377da900c2679dc50bb5
윤명식 <yms0059@naver.com>
2016-08-25 21:28:29 +0900

    <명식> 블루투스 권한추가밑 아두이노와 블루투스 데이터 주고받기 기능완성

--- app/src/main/java/ensharp/goldensignal/BluetoothRegister.java
+++ app/src/main/java/ensharp/goldensignal/BluetoothRegister.java
@@ -2,6 +2,14 @@ package ensharp.goldensignal;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 
 public class BluetoothRegister extends Activity {
 
@@ -9,5 +17,10 @@ public class BluetoothRegister extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_register);
+    }
+    public void onOpenClientSocketButtonClicked(View view)
+    {
+        Intent enabler = new Intent(this, ClientSocketActivity.class);
+        startActivity(enabler);
     }
 }
